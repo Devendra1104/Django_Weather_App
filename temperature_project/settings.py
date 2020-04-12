5c2f96eeb3918a72cecda61bc14b3298070f1c5c
Devendra1104 <devendrapanchal58@gmail.com>
2020-04-12 12:59:29 +0530

    New updates for Heroku deployment

--- temperature_project/settings.py
+++ temperature_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = ')6foks-q5@u5(xz0+m5!=bs*#l+n1o*f5mp=@8nhs8nqf$%igh'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
