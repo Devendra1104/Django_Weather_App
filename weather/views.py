fc84dbe18e203386cba87376e6f58dbd6c9d47c1
Devendra1104 <devendrapanchal58@gmail.com>
2020-04-12 12:42:09 +0530

    Modified Weather views

--- weather/views.py
+++ weather/views.py
@@ -5,7 +5,7 @@ import urllib.request
 def home(request):
     if request.method == 'POST':
         city = request.POST['city']
-        key = 'APIKEY'
+        key = '3d2bef9c52ea4b95b351620f3c8a005d'
         url = urllib.request.urlopen('https://api.weatherbit.io/v2.0/current?city='+city+'&key='+key).read()
         j_response = json.loads(url)
         temperature = j_response['data'][0]['temp']
